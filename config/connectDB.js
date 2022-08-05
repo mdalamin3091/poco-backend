3d0fbe128e510fb45a174ab0ef823f839b5f6a77
akalamin <akalaminamin91@gmail.com>
2022-08-05 08:48:20 -0700

    change database url path

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 // const db = `mongodb://localhost:27017/poco`;
-const { DB_PASS, DB_USER } = require("../config/envConfig");
-// const db = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.deaij.mongodb.net/poco?retryWrites=true&w=majority`;
-const db = `mongodb+srv://alamin:maw997i0UDGmYlhm@cluster0.lq4b1.mongodb.net/poco?retryWrites=true&w=majority`;
+const { DB_URL } = require("../config/envConfig");
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(DB_URL);
     console.log("Connection successfull");
   } catch (error) {
     console.log(error);
--- config/envConfig.js
+++ config/envConfig.js
@@ -4,5 +4,6 @@ module.exports = {
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
   JWT_SECRET: process.env.JWT_SECRET,
-  STRIPE_SECRET:process.env.STRIPE_SECRET
+  STRIPE_SECRET:process.env.STRIPE_SECRET,
+  DB_URL:process.env.DB_URL
 };
