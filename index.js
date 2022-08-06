d44cd2ba648311d1f99b9a570922732b9271566a
akalamin <akalaminamin91@gmail.com>
2022-08-06 11:47:43 -0700

    update something

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectDB = require("./config/connectDB");
-const authRoutes = require("./routes/authRoutes")
-const categoryRoutes = require("./routes/categoryRoutes")
+const authRoutes = require("./routes/authRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 const productRoutes = require("./routes/productRoutes");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+
 // parsor
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // connect DB
-connectDB()  
+connectDB();
 
 // routes 
-app.use('/api/v1/auth', authRoutes) 
-app.use('/api/v1/category', categoryRoutes)   
-app.use('/api/v1/product', productRoutes) 
+app.use('/api/v1/auth', authRoutes); 
+app.use('/api/v1/category', categoryRoutes);   
+app.use('/api/v1/product', productRoutes); 
 
-app.get('/', (req, res) => res.send('In the name of Allah'))
+app.get('/', (req, res) => res.send('In the name of Allah'));
  
 app.listen(port, () => console.log(`server running port: ${port}`));  
